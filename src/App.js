56ee423cd615eb65c58b90aea265e026c4baca95
trieule50 <trieule50@gmail.com>
2021-07-06 21:47:38 -0400

    Navigation Bar Added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
-import Nav from './Components/Nav';
+import Navigation from './Components/Nav';
 import About from './Components/About';
 import Portfolio from './Components/Portfolio';
 import Contact from './Components/Contact';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <Nav/>
+        <Navigation/>
       </header>
       <main>
-        <About/>
-        <Portfolio/>
-        <Contact/>
+        <Route path='/about' exact render={()=> <About/>}/>
+        <Route path='/portfolio' exact render={()=> <Portfolio/>}/>
+        <Route path='/contact' exact render={()=><Contact/>}/>
+        <Route path='/' exact render={()=><Home/>}/>
       </main>
       <footer></footer>
     </div>
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,6 +1,30 @@
-export default function Nav(){
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+
+export default function Navigation(){
     return(
-        <>
-        </>
+        <Navbar className="nav" collapseOnSelect variant="light" expand="md">
+        <Navbar.Brand as={Link} to="/"><h1 className='favicon'>
+            T</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} to="/">
+            <h2 className='navTitle'>Home</h2>
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+            <h2 className='navTitle'>About</h2>
+            </Nav.Link>
+            <Nav.Link as={Link} to="/portfolio">
+            <h2 className='navTitle'>Portfolio</h2>
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+            <h2 className='navTitle'>Contact Me</h2>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
 }

4896097c8df56948aff57b1898ee5f3272b31272
trieule50 <trieule50@gmail.com>
2021-07-06 21:35:00 -0400

    Components Created and Linked

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Nav from './Components/Nav';
+import About from './Components/About';
+import Portfolio from './Components/Portfolio';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Nav/>
       </header>
+      <main>
+        <About/>
+        <Portfolio/>
+        <Contact/>
+      </main>
+      <footer></footer>
     </div>
   );
 }

0c9f685f8e2fc05bd1d7a9e08d833b0468705ff2
trieule50 <trieule50@gmail.com>
2021-07-11 19:06:55 -0400

    Edits to About Page

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,3 +1,6 @@
+import Resume from '../About/Trieu Le - Resume - 2021.pdf';
+import ResumeLogo from '../About/resume_icon.png';
+
 export default function About(){
     return(
         <div className='about'>
@@ -5,7 +8,18 @@ export default function About(){
             <p>I am a software engineer with a background in engineering. As a civil engineer, I enjoy solving problems in the most unorthodox way and creating projects that the general public uses everyday. During my time in project management, I became very passionate about technology more specifically, artificial intelligence in construction.  And on May 2021, I made a decision to study software engineering in hope to make a bigger impact on society.</p>
             <div className='skills'>
                 <div className='skillsItems'>
-                    <a href='https://drive.google.com/file/d/1fbMjaIJF5GjVUQnpmDS0Dq6KpAxYr2Qy/view?usp=sharing' target='_blank' rel='noopener noreferrer'><div className='resume'>Click here for resume</div></a>
+                    <a href={Resume} target='_blank' rel='noopener noreferrer'>
+                        <div className='resume'><img src={ResumeLogo} alt='Click for Resume'
+                        style={{
+                            height:'10rem'
+                        }}/>
+                        <div className='overlay'>
+                            <div className='text'>
+                                Click to View Resume
+                            </div>
+                        </div>
+                        </div>
+                    </a>
                 </div>
                 <div className='skillsItems'>
                     <h4>List of Skills</h4>

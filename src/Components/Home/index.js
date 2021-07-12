a51ab38500e782b3f37d345c9a9de29df66df75e
trieule50 <trieule50@gmail.com>
2021-07-11 20:10:27 -0400

    Landing page Completed

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,6 +1,15 @@
+import PortfolioImage from '../Home/Portfolio.png.001.png';
+import Trieu from '../Home/TL.png';
+
 export default function Home(){
     return(
-        <div className='home'>
+        <div className='home' style={{
+            background:`url(${Trieu}), url(${PortfolioImage})`,
+            backgroundRepeat: 'no-repeat, no-repeat',
+            backgroundPosition: 'right bottom, center',
+            backgroundSize: '30rem 40rem, 95rem 50rem',
+            // backgroundAttachment: 'fixed'
+        }}>
             <h1 className='landingTitle'>Builder &nbsp; to &nbsp;<span> {'<'}Coder{'>'}</span></h1>
         </div>
     )
